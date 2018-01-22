e9cd04dd7131df827066e3d2349e511c968e1b12
Chris Lennon <chrislennon@users.noreply.github.com>
2018-01-22 10:53:15 +0100

    Auto-generated commit

--- data/apiSchema.js
+++ data/apiSchema.js
@@ -1,12 +1,12 @@
 const APIPrice = function() {
     return {
         "live" : {
-            "response" : "import urllib2,json,sys\n\ndata = urllib2.urlopen(\"https:\/\/min-api.cryptocompare.com\/data\/pricemultifull?fsyms=**CRYPTO**&tsyms=**FIAT**\")\nobj=json.load(data)\ncurrent = \"{**FORMAT**}\".format(obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"PRICE\"])\nopening = obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"OPEN24HOUR\"]\nhigh = \"{**FORMAT**}\".format(obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"HIGHDAY\"])\nlow = \"{**FORMAT**}\".format(obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"LOWDAY\"])\nraw_current = obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"PRICE\"]\nraw_high = obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"HIGHDAY\"]\nraw_low = obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"LOWDAY\"]\n\n",
+            "response" : "import urllib2,json,sys\n\ndata = urllib2.urlopen(\"https:\/\/min-api.cryptocompare.com\/data\/pricemultifull?fsyms=**CRYPTO**&tsyms=**FIAT**\")\nobj=json.load(data)\ncurrent = \"{**FORMAT**}\".format(obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"PRICE\"])\nopening = obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"OPEN24HOUR\"]\nhigh = \"{**FORMAT**}\".format(obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"HIGHDAY\"])\nlow = \"{**FORMAT**}\".format(obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"LOWDAY\"])\nraw_current = float(obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"PRICE\"])\nraw_opening = float(obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"OPEN24HOUR\"])\nraw_high = float(obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"HIGHDAY\"])\nraw_low = float(obj[\"RAW\"][\"**CRYPTO**\"][\"**FIAT**\"][\"LOWDAY\"])\n\n",
             "no-output" : "print(\"**FIATSYMB**\" + current)",
-            "simple-output" : "if (raw_current > opening):\n\ttrend = \"▲\"\nelse:\n\ttrend = \"▼\"\n\nprint(\"**FIATSYMB**\" + current + \" \" + trend)",
+            "simple-output" : "if (raw_current > raw_opening):\n\ttrend = \"▲\"\nelse:\n\ttrend = \"▼\"\n\nprint(\"**FIATSYMB**\" + current + \" \" + trend)",
             "absolute-output" : "print (\"**FIATSYMB**\" + current + \" (L: **FIATSYMB**\" + low + \" H: **FIATSYMB**\" + high + \")\")",
             "relative-output" : "print(\"**FIATSYMB**\" + current + \" (L: -**FIATSYMB**\" + str(raw_current - raw_low) + \" H: +**FIATSYMB**\" + str(raw_high - raw_current) + \")\")",
-            "current-percentage-output" : "print (\"**FIATSYMB**\" + current + \" (\" + str(round (((raw_current - opening) \/ raw_current) * 100)) + \"%)\")",
+            "current-percentage-output" : "print (\"**FIATSYMB**\" + current + \" (\" + str(round (((raw_current - raw_opening) \/ raw_current) * 100)) + \"%)\")",
             "range-percentage-output" : "print(\"**FIATSYMB**\" + current + \" (L: -\" + str(round (((raw_current - raw_low) \/ raw_current) * 100)) + \"% H: +\" + str(round (((raw_high - raw_current) \/ raw_current) * 100)) + \"%)\")",
             "user-percentage-output" : "print(\"**FIATSYMB**\" + current + \" (L: **FIATSYMB**\" + str(raw_current - (raw_current * **PERCENT**)) + \" H: **FIATSYMB**\" + str(raw_current + (raw_current * **PERCENT**)) + \")\")"
         },
